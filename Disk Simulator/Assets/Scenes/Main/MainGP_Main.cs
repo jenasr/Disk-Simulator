1fba15e0a30af3452febf284a68c8867801ff16a
LAPTOP-U752MJ4H\dumba <austng@gmail.com>
2022-05-03 14:40:19 -0700

    Main Gameplay Scene

---
+++ Disk Simulator/Assets/Scenes/Main/MainGP_Main.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using YuGiOh;
+
+public class MainGP_Main : MonoBehaviour {
+    public GameObjectReferences references;
+
+    GameStateController gsc;
+    Game g;
+    GameActionStack gas;
+
+    void Start() {
+        g = new Game();
+        gas = new GameActionStack();
+        gsc = new GameStateController(g, gas, references);
+
+        references.backgroundButton.onClick.AddListener(() => InputManager.Set.CancelRequested());
+        references.cardActionMenuBehaviour.OnActionSelect += (a) => {
+            InputManager.Set.ActionRequested(a);
+        };
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            InputManager.Set.CardScanned(32339440);
+        }
+        if (Input.GetKeyDown(KeyCode.P)) {
+            InputManager.Set.ActionRequested(SetPlayerTurnAction.Get(g, (g.currentPlayer + 1) % g.players.Length));
+        }
+        gsc.Next();
+        InputManager.Clear();
+    }
+}
